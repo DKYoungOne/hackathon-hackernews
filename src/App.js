8addd2e4b4d0840b1aae828a2d8ca8859a90eaba
DKYoungOne <bawsinator@gmail.com>
2023-05-22 19:43:47 -0500

    add project files after repo creation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      query: "tags=front_page",
+      searchResults: [],
+    }
+  }
+
+  onChangeHandler = ({ target }) => {
+    console.log(target.value);
+    this.setState({
+      query: target.value,
+    })
+  }
+
+  // http://hn.algolia.com/api/v1/search?query=${this.state.query}}&tags=story
+
+  componentDidMount() {
+    fetch(`http://hn.algolia.com/api/v1/search?query=${this.state.query}&tags=story`)
+      .then(res => {
+        return res.json()
+      }).then(data => {
+        console.log(data)
+        this.setState({
+          searchResults: data.hits
+        })
+      })
+  }
+
+  componentWillUpdate(prevProps, prevState) {
+    console.log(prevState);
+    // if (!this.state.searchResults[0]?.objectID || !prevState.searchResults[0]?.objectID) return;
+
+    if (this.state.query !== prevState.query) {
+      console.log('they matchhhhhhhhhhhhhh')
+      fetch(`http://hn.algolia.com/api/v1/search?query=${this.state.query}&tags=story`)
+        .then(res => {
+          return res.json()
+        }).then(data => {
+          console.log(data)
+          this.setState({
+            searchResults: data.hits
+          })
+        })
+      return;
+    }
+    return;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="Search-header">
+          <span>
+            <img class="Logo-Image" alt="White H inside a white outlined square with an orange background" src="https://hn.algolia.com/packs/media/images/logo-hn-search-a822432b.png" />
+            <span class="Logo-Label">
+              Search
+              <br />
+              Hacker News
+            </span>
+          </span>
+          <input class="Search-Bar" type="text" onChange={(e) => this.onChangeHandler(e)}></input>
+        </header>
+        <main>
+          <ul>
+            {this.state.searchResults.map((res, idx) => {
+              return (
+                <li key={idx}>
+                  <a href={res.url} target='_blank' rel='noreferrer'>{res.title}{' '}</a>
+                </li>
+              )
+
+            })}
+          </ul>
+        </main>
+      </div>
+    );
+  }
+
 }
 
 export default App;
